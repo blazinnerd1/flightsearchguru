15f05f1bd091fe834d108f362350d9590c8afc47
Logan <hirelogan+github@gmail.com>
2018-08-08 19:54:05 -0500

    enable graphiql endpoint (w/ hard-coded mock data)
    added dotenv
    throw error if env vars not found

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
+require('dotenv').config()
 const express = require('express');
 const compression = require('compression');
+const graphqlHTTP = require('express-graphql');
+const schema = require('../database/schema.js');
 
 const app = express();
-const port = process.env.PORT || 3301;
+const port = process.env.PORT;
+
+if (!port) {
+  throw new Error('Environment variables not found!');
+}
 
 app.use(compression());
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -13,4 +25,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`express is listening on ${port}`);
 });
-  // .then(() => console.log(`express is listening on ${port}`));
