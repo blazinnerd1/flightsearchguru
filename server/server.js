5e8d05cb6793fe12f7e41718ba110e40f5b954c9
Logan <hirelogan+github@gmail.com>
2018-08-09 10:18:25 -0500

    add missing semi

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const compression = require('compression');
+const graphqlHTTP = require('express-graphql');
+const schema = require('../database/schema.js');
+
+const app = express();
+const port = process.env.PORT;
+
+if (!port) {
+  throw new Error('Environment variables not found!');
+}
+
+app.use(compression());
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+}));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`express is listening on ${port}`);
+});
