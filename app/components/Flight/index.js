7d4134a56e9a1645d79a8cb55779106fa87d64ce
Scott <scottgrimes89@gmail.com>
2018-08-13 19:15:56 -0500

    add time to flight objects

--- app/components/Flight/index.js
+++ app/components/Flight/index.js
@@ -18,7 +18,7 @@ import messages from './messages';
 /* eslint-disable react/prefer-stateless-function */
 class Flight extends React.Component {
   render() {
-    const { from_id, to_id, departing, price,info  } = this.props.flight;
+    const { from_id, to_id, departing, price, info  } = this.props.flight;
     const linkDest = `https://www.kayak.com/flights/${from_id}-${to_id}/${departing}?sort=bestflight_a`;
     const airportsProps = {from_id, to_id}
     return <div style={{ display: 'flex', width: '80%', minWidth: '500px', border: '1px solid grey', backgroundColor: 'white', verticalAlign: 'center', margin: '5px', padding: '5px', justifyContent: 'center', alignItems: 'center' }}>
--- app/containers/FlightResults/index.js
+++ app/containers/FlightResults/index.js
@@ -13,11 +13,22 @@ import messages from './messages';
 const {airports} = require('../../../data/data')
 
 const data = [];
+
+// 50% of flights have one segment. the other 50% have 3 segments
+const getFakeFlightInfo = () => {
+  if (Math.random() > .5) {
+    return `[{"DepartureAirport":{"LocationCode":"AUS"},"ArrivalAirport":{"LocationCode":"LGW"},"Equipment":{"AirEquipType":"789"},"FlightCabin":{"CabinType":"Coach"},"FlightClass":{"ClassType":"V"},"MarketingAirline":{"Code":"DI"},"OperatedByAirline":{"CompanyText":"DI"},"Flight":{"FlightType":"O"},"OpaqueType":"0","ShowBaggageAlert":false,"StopAirports":null,"FlightNumber":"7182","DepartureDateTime":"06Sep2018T04:20 PM","ArrivalDateTime":"07Sep2018T07:25 AM","FlightDuration":"09.05","FDType":"T","StopQuantity":0,"RPH":1,"CurrentBrandName":null,"UpgradedBrandName":null,"IsUpgradable":false}]`;
+  } else {
+    return `[{"DepartureAirport":{"LocationCode":"AUS"},"ArrivalAirport":{"LocationCode":"JFK"},"Equipment":{"AirEquipType":"320"},"FlightCabin":{"CabinType":"Coach"},"FlightClass":{"ClassType":"K"},"MarketingAirline":{"Code":"FI"},"OperatedByAirline":{"CompanyText":"JetBlue Airways"},"Flight":{"FlightType":"O"},"OpaqueType":"0","ShowBaggageAlert":false,"StopAirports":null,"FlightNumber":"7808","DepartureDateTime":"06Sep2018T01:44 PM","ArrivalDateTime":"06Sep2018T06:30 PM","FlightDuration":"3.46","FDType":"S","StopQuantity":0,"RPH":1,"CurrentBrandName":null,"UpgradedBrandName":null,"IsUpgradable":false},{"DepartureAirport":{"LocationCode":"JFK"},"ArrivalAirport":{"LocationCode":"KEF"},"Equipment":{"AirEquipType":"76W"},"FlightCabin":{"CabinType":"Coach"},"FlightClass":{"ClassType":"K"},"MarketingAirline":{"Code":"FI"},"OperatedByAirline":{"CompanyText":"FI"},"Flight":{"FlightType":"O"},"OpaqueType":"0","ShowBaggageAlert":false,"StopAirports":null,"FlightNumber":"614","DepartureDateTime":"06Sep2018T08:20 PM","ArrivalDateTime":"07Sep2018T05:55 AM","FlightDuration":"5.35","FDType":"S","StopQuantity":0,"RPH":2,"CurrentBrandName":null,"UpgradedBrandName":null,"IsUpgradable":false},{"DepartureAirport":{"LocationCode":"KEF"},"ArrivalAirport":{"LocationCode":"LGW"},"Equipment":{"AirEquipType":"75W"},"FlightCabin":{"CabinType":"Coach"},"FlightClass":{"ClassType":"K"},"MarketingAirline":{"Code":"FI"},"OperatedByAirline":{"CompanyText":"FI"},"Flight":{"FlightType":"O"},"OpaqueType":"0","ShowBaggageAlert":false,"StopAirports":null,"FlightNumber":"470","DepartureDateTime":"07Sep2018T07:45 AM","ArrivalDateTime":"07Sep2018T11:45 AM","FlightDuration":"3.0","FDType":"S","StopQuantity":0,"RPH":3,"CurrentBrandName":null,"UpgradedBrandName":null,"IsUpgradable":false}]`;
+  }
+
+
+}
 const makeFakeData = ()=>{
   while(data.length<50){
     const price = Math.floor(Math.random()*200+200);
     const day = Math.floor(Math.random()*12+10)
-    const temp = { from_id: 'IAD', to_id: 'AUH', departing: `2018-09-${day}`, price };
+    const temp = { from_id: 'IAD', to_id: 'AUH', departing: `2018-09-${day}`, price, info: getFakeFlightInfo()};
       data.push(temp);
   }
 }
