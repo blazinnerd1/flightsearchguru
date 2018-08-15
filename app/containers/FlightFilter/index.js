0e14c3dcf054cd452e4829419d389cea9d4857e0
Scott <scottgrimes89@gmail.com>
2018-08-15 12:16:07 -0500

    init filter

--- app/containers/FlightFilter/index.js
+++ app/containers/FlightFilter/index.js
@@ -11,6 +11,7 @@ import { Helmet } from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
+import { makeSelectFlightResults } from '../SearchBar2/selectors';
 
 import injectReducer from 'utils/injectReducer';
 import makeSelectFlightFilter from './selectors';
@@ -19,6 +20,16 @@ import messages from './messages';
 
 /* eslint-disable react/prefer-stateless-function */
 export class FlightFilter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dirty: false, // if a filter has been changed but not saved the filter is dirty
+      filterByPrice: false,
+      priceLow: 0,
+      priceHigh: 0,
+      sortBy: 'cheapest', // can be cheapest or date
+    };
+  }
   render() {
     return (
       <div style={{ width: '50px' }}>
@@ -30,6 +41,7 @@ export class FlightFilter extends React.Component {
         <div>Filter by arrival time</div>
         <div>Filter by price (cool component)</div>
         <div>sort by cheapest or date </div>
+        GO
       </div>
     );
   }
@@ -41,6 +53,7 @@ FlightFilter.propTypes = {
 
 const mapStateToProps = createStructuredSelector({
   flightfilter: makeSelectFlightFilter(),
+  searchResults: makeSelectFlightResults(),
 });
 
 function mapDispatchToProps(dispatch) {
