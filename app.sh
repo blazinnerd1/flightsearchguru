489d41ab79197d35ea3bcd4496ac3534219a8dc5
Scott <scottgrimes89@gmail.com>
2018-08-20 10:16:37 -0500

    add shell script

--- app.sh
+++ app.sh
@@ -1,6 +1,6 @@
 if [ "$1" == "client" ]; then
 	echo "Rebuilding client files and restarting container..!"
-    npm run build && docker stop fsguru && docker rm fsguru && docker build -t fsguru . && docker run --name fsguru --network br0 -d fsguru 
+    docker stop fsguru && docker rm fsguru && docker build -t fsguru . && docker run --name fsguru --network br0 -d fsguru 
 elif [ "$1" == "nocontainer" ]; then
     echo "Starting new container.."
     docker build -t fsguru . && docker run --name fsguru --network br0 -d fsguru
