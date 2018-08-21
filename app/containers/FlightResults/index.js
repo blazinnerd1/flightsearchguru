fedc26950a689d76bf7374cd2aa92856c82a5dc7
Logan <hirelogan+github@gmail.com>
2018-08-21 16:40:10 -0500

    refactor
    Change UI wording for clarity
    Condense multiple sorting functions into one
    Comment out unused code

--- app/containers/FlightResults/index.js
+++ app/containers/FlightResults/index.js
@@ -7,12 +7,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { FormattedMessage } from 'react-intl';
+// import { FormattedMessage } from 'react-intl';
 import LoadingIndicator from 'components/LoadingIndicator';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 import FlightList from 'components/FlightList';
 import FlightFilter from 'containers/FlightFilter';
+import Map from 'components/LeafletMap';
 import {
   makeSelectShouldRenderSearchResults,
   makeSelectFilteredFlights,
@@ -26,26 +27,19 @@ import { APPLY_NEW_FILTER } from 'containers/SearchBar2/constants';
 import { changeView } from 'containers/SearchBar2/actions';
 import FlightListGraph from 'components/FlightListGraph';
 
-import messages from './messages';
+// import messages from './messages';
 
 /* eslint-disable react/prefer-stateless-function */
 export class FlightResults extends React.Component {
   constructor(props) {
     super(props);
-    this.onFilterByDeparture = this.onFilterByDeparture.bind(this);
-    this.onFilterByPrice = this.onFilterByPrice.bind(this);
+    this.sortBy = this.sortBy.bind(this);
     this.showView = this.showView.bind(this);
   }
-  onFilterByPrice() {
-    const { sortBy, ...rest } = this.props.filters.toObject();
-    console.log('triggered price');
-    this.props.refilter({ sortBy: 'cheapest', ...rest });
-  }
 
-  onFilterByDeparture() {
+  sortBy(criteria) {
     const { sortBy, ...rest } = this.props.filters.toObject();
-    console.log('triggered departure');
-    this.props.refilter({ sortBy: 'departure', ...rest });
+    this.props.refilter({ sortBy: criteria, ...rest });
   }
 
   showView(view) {
@@ -63,9 +57,10 @@ export class FlightResults extends React.Component {
 
     // flights is the filtered flights
     // searchResults is the unfiltered flights
-
     if (!shouldDisplayResults) {
-      return <div>I should be a landing page</div>;
+      return (
+        <div>Consult the guru above and your flights will appear here.</div>
+      );
     }
 
     if (isLoading) {
@@ -73,28 +68,31 @@ export class FlightResults extends React.Component {
     }
 
     if (hasError) {
-      return <div>Error! Please try again</div>;
+      return <div>Error! Please try again.</div>;
     }
 
     if (!flights || !flights.length) {
-      return <div>No Flights Found</div>;
+      return <div>No flights found.</div>;
     }
-
 
     let display = <FlightList flights={flights} />;
     if (view === 'map') {
-      display = <div>I'm the map hergin dergin</div>;
+      display = <Map flights={flights} />;
     } else if (view === 'graph') {
       display = <FlightListGraph flights={flights} />;
-    } 
-    
+    }
+
     return (
       <div>
         <div style={{ position: 'relative', left: '0' }}>
-          <button onClick={this.onFilterByPrice}>Price</button>{' '}
-          <button onClick={this.onFilterByDeparture}>Departure</button>
+          Sort:
+          <button onClick={() => this.sortBy('cheapest')}>Price</button>{' '}
+          <button onClick={() => this.sortBy('departure')}>
+            Earliest Departure
+          </button>
         </div>
         <div style={{ position: 'relative', right: '0' }}>
+          View:
           <button onClick={() => this.showView('list')}>List</button>
           <button onClick={() => this.showView('map')}>Map</button>{' '}
           <button onClick={() => this.showView('graph')}>Graph</button>
@@ -115,8 +113,7 @@ FlightResults.propTypes = {
   hasError: PropTypes.bool,
   updateView: PropTypes.func,
   view: PropTypes.string,
-  updateFilter: PropTypes.func,
-
+  // updateFilter: PropTypes.func,
 };
 
 export function mapDispatchToProps(dispatch) {
