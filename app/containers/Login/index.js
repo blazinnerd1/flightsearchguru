14c7139e1b5d086747f6ce46af0e80eb811be629
Scott <scottgrimes89@gmail.com>
2018-08-22 16:25:28 -0500

    Style login button

--- app/containers/Login/index.js
+++ app/containers/Login/index.js
@@ -18,7 +18,33 @@ import reducer from './reducer';
 import saga from './saga';
 import { GOOGLE_CLIENT_ID } from '../../../config';
 import GoogleLogin from 'react-google-login';
+import styled from 'styled-components';
 /* eslint-disable react/prefer-stateless-function */
+
+const LoginImage = styled.button`
+  display: inline-flex;
+  padding: 0.25em 2em;
+  margin: 1em;
+  text-decoration: none;
+  border-radius: 4px;
+  -webkit-font-smoothing: antialiased;
+  -webkit-touch-callout: none;
+  user-select: none;
+  cursor: pointer;
+  outline: 0;
+  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-weight: bold;
+  font-size: 16px;
+  border: 2px solid #41addd;
+  color: #41addd;
+
+  &:active {
+    background: #41addd;
+    color: #fff;
+  }
+`;
+
+const LogoutImage = styled.img``;
 
 export class Login extends React.Component {
   constructor(props) {
@@ -43,11 +69,9 @@ export class Login extends React.Component {
         onFailure={this.handleFailure}
         style={{}}
       >
-        <img
-          style={{ width: '150px' }}
-          src="images/btn_google_signin_light_normal_web@2x.png"
-          alt="Login with Google"
-        />
+        <LoginImage>
+          <img width="23px" height="23px" src="images/googleIcon.png" />Login
+        </LoginImage>
       </GoogleLogin>
     );
   }
