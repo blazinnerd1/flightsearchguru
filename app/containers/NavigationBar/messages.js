d620ebf40996cfa8277707f8dde2dc9bdbe816fe
Scott <scottgrimes89@gmail.com>
2018-08-25 15:30:38 -0500

    navigation bar

---
+++ app/containers/NavigationBar/messages.js
@@ -0,0 +1,14 @@
+/*
+ * Menu Messages
+ *
+ * This contains all the text for the Menu component.
+ */
+
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'app.containers.Menu.header',
+    defaultMessage: 'flight search guru',
+  },
+});
