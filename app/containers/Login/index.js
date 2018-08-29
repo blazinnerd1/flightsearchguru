490c9ad2dea19e800c502a0e3019a04a4c93f2f3
Clayton Lin <clayton.yt.lin@gmail.com>
2018-08-28 19:16:22 -0500

    Test create price alert

--- app/containers/Login/index.js
+++ app/containers/Login/index.js
@@ -37,7 +37,6 @@ export class Login extends React.Component {
     // hydrate with saved session if it exists
     const session_id = localStorage.getItem('session_id');
 
-
     // // USED FOR DEV TESTING
     // const session_id = '12345678';
 
