89a916a3b5f4efdef1e72bafbc3acb7be24b7897
Scott <scottgrimes89@gmail.com>
2018-08-30 16:18:52 -0500

    final cleanup

--- app/deleteme/Explore/tests/reducer.test.js
+++
@@ -1,8 +0,0 @@
-import { fromJS } from 'immutable';
-import exploreReducer from '../reducer';
-
-describe('exploreReducer', () => {
-  it('returns the initial state', () => {
-    expect(exploreReducer(undefined, {})).toEqual(fromJS({}));
-  });
-});
